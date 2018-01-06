6a61ae1c37b768a234e46241b5422123725b01f2
Rahul <rahulsharma841990@outlook.com>
2018-01-07 02:04:33 +0530

    update contact page

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -1,4 +1,6 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 // check if fields passed are empty
 if(empty($_POST['name'])  		||
    empty($_POST['phone']) 		||
@@ -19,8 +21,57 @@ $message = $_POST['message'];
 $to = 'info@lifelongsolar.in'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "LifeLong Solar Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@lifelongsolar.in\n";
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+    
+    
+
+    require '../classes/config.php';
+
+    //Load composer's autoloader
+    require '../vendor/autoload.php';
+
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = HOST;  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = EMAIL;                 // SMTP username
+        $mail->Password = PASSWORD;                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+        // $mail->SMTPDebug = 1;
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        //Recipients
+        $mail->setFrom('info@lifelongsolar.in', 'Life Long Solar');
+        $mail->addAddress($to, 'Life Long Solar');     // Add a recipient
+        $mail->addAddress($to);               // Name is optional
+        $mail->addReplyTo($email_address, 'Life Long Solar');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $email_subject;
+        $mail->Body    = $email_body;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+        return true;
+    } catch (Exception $e) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }
+
 ?>
