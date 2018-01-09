8736b12762390969c36c0b24af29891136995877
Rahul <rahulsharma841990@outlook.com>
2018-01-10 00:07:03 +0530

    fix smaal changes

--- 404.php
+++ 404.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en"><!-- InstanceBegin template="/Templates/inner.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
+    
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
